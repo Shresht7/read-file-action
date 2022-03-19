9d7ca7dafa85f9ea19534c0f7e6abe3460d6b3e8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-19 19:12:56 +0530

    Read and output file

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,19 @@
 //  Library
 import * as core from '@actions/core'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { src, workspace } from './config'
+import { outputs } from './metadata'
 
 //  ======
 //  ACTION
 //  ======
 
 async function action() {
-    core.notice('Hello World!')
+    const filePath = path.join(workspace, src)
+    const contents = await fs.promises.readFile(filePath, { encoding: 'utf-8' })
+    console.log(contents)
+    core.setOutput(outputs.contents, contents)
 }
 
 //  -----------------
