220aeea40ae7448ee99d981fc8a12ba399c38dbc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-04-02 12:34:18 +0530

    Remove dependence on GITHUB_WORKSPACE

--- src/config.ts
+++ src/config.ts
@@ -7,14 +7,6 @@ import { inputs } from './metadata'
 //  CONFIG
 //  ======
 
-
-if (!process.env.GITHUB_WORKSPACE) {
-    throw new Error('Invalid GITHUB_WORKSPACE. You need to checkout this repository using the actions/checkout@v3 github-action for the GITHUB_WORKSPACE environment variable')
-}
-
-/** GitHub Workspace URL */
-export const workspace = process.env.GITHUB_WORKSPACE
-
 /** Path to the source file with markdown-slots */
 export const src = core.getInput(inputs.path, { required: true })
 
--- src/helpers/readFile.ts
+++ src/helpers/readFile.ts
@@ -2,7 +2,6 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { isURL } from './isURL'
-import { workspace } from '../config'
 
 /** Read file from the given path or url */
 export async function readFile(src: string) {
@@ -25,6 +24,5 @@ function readFromURL(url: string): Promise<string> {
 
 /** Read from the given path */
 function readFromFile(src: string): Promise<string> {
-    const srcPath = path.join(workspace, src)
-    return fs.promises.readFile(srcPath, { encoding: 'utf-8' })
+    return fs.promises.readFile(src, { encoding: 'utf-8' })
 }
